b89f2b87d14c7bba51ba6257e0eb173779b9585f
dlromero <raiga1234@gmail.com>
2017-06-28 21:49:29 +0000

    API

--- src/app/services/achievements.service.ts
+++ src/app/services/achievements.service.ts
@@ -4,7 +4,7 @@ import { Http, Response } from '@angular/http';
 
 import { Observable } from 'rxjs/Observable';
 
-export const API_Marvel_URL = "https://gateway.marvel.com:443/v1/public/characters?limit=15&ts=1&apikey=d72597496826cb29fc30c4b132f63abf&hash=e47c310d884b3e636ff018938570605e";
+export const API_Marvel_URL = "https://gateway.marvel.com:443/v1/public/characters?limit=50&ts=1&apikey=d72597496826cb29fc30c4b132f63abf&hash=e47c310d884b3e636ff018938570605e";
 
 @Injectable()
 export class AchievementsService {
